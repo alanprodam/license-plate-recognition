da9ddeccda1935bcfb2b22b75c538b228b3abd3c
Alan Tavares <alan.am.tavares@gmail.com>
2021-05-21 17:20:00 -0300

    Atualizado - area de concentração dos carros

--- rtsp_detection_car.py
+++ rtsp_detection_car.py
@@ -2,6 +2,7 @@ import cv2
 import timeit
 import math
 import imutils
+import numpy as np
 from imutils import paths
 
 def drawText(frame, scale, rectX, rectY, rectColor, text):
@@ -10,9 +11,12 @@ def drawText(frame, scale, rectX, rectY, rectColor, text):
     top = max(rectY - rectThinkness, textSize[0])
 
     cv2.putText(frame, text, (rectX, top), cv2.FONT_HERSHEY_SIMPLEX, scale, rectColor, 3)
-
 def calculate_centr(coord):
     return (coord[0] + (coord[2] / 2), coord[1] + (coord[3] / 2))
+
+
+def calculate_centr_cut(coord):
+    return (int(abs(coord[0] - coord[1]) / 2 + coord[0]), int(abs(coord[2] - coord[3]) / 2 + coord[2]))
 
 
 def calculate_centr_distances(centroid_1, centroid_2):
@@ -56,14 +60,14 @@ if __name__ == '__main__':
     #     'data/vehicle-detection-0201/FP32/vehicle-detection-0201.bin')
 
     # # Load the model vehicle-detection-0202 (Identifica veiculos) 512x512
-    # net = cv2.dnn.Net_readFromModelOptimizer(
-    #     'data/vehicle-detection-0202/FP32/vehicle-detection-0202.xml',
-    #     'data/vehicle-detection-0202/FP32/vehicle-detection-0202.bin')
+    net = cv2.dnn.Net_readFromModelOptimizer(
+        'data/vehicle-detection-0202/FP32/vehicle-detection-0202.xml',
+        'data/vehicle-detection-0202/FP32/vehicle-detection-0202.bin')
 
     # Load the model detection-crossroad-1016 (Pessoas, veiculos e bicicletas) 512x512
-    net = cv2.dnn.Net_readFromModelOptimizer(
-        'data/person-vehicle-bike-detection-crossroad-1016/FP32/person-vehicle-bike-detection-crossroad-1016.xml',
-        'data/person-vehicle-bike-detection-crossroad-1016/FP32/person-vehicle-bike-detection-crossroad-1016.bin')
+    # net = cv2.dnn.Net_readFromModelOptimizer(
+    #     'data/person-vehicle-bike-detection-crossroad-1016/FP32/person-vehicle-bike-detection-crossroad-1016.xml',
+    #     'data/person-vehicle-bike-detection-crossroad-1016/FP32/person-vehicle-bike-detection-crossroad-1016.bin')
 
 
     # Specify target device
@@ -83,8 +87,12 @@ if __name__ == '__main__':
     print("shape: ", first_frame.shape)  # 1080x1920x3
 
     color_font = (0, 0, 0)
-    color_red = (255, 0, 0)
+    color_red = (0, 0, 255)
+    color_blue = (255, 0, 0)
+    radius = 2
+    thickness = 4
 
+    array_center_car = []
     while (True):
         ############## Time ##############
         start = timeit.default_timer()
@@ -95,6 +103,8 @@ if __name__ == '__main__':
         # -- Capture frame-by-frame
         # plateRecognition(frame)
 
+
+
         # name: "input" , shape: [1x3x300x300] - An input image in the format [BxCxHxW], where:
         # Chama primeira rede neural referente detecção do veículo
         # blob = cv2.dnn.blobFromImage(frame, size=(300, 300), ddepth=cv2.CV_8U)
@@ -103,47 +113,65 @@ if __name__ == '__main__':
         net.setInput(blob)
         out = net.forward()
 
+        region_x = int(1080/2)
+        region_y = int(1920/2)
         # Recorte do veiculo identificado
-        cuttImg = frame[0:300 + 1, 0:300 + 1]
+        cuttImg = frame[region_x:1080, region_y:1920]
 
+        listDetections = []
         # The net outputs a blob with the shape: [1, 1, N, 7], where N is the number of detected bounding boxes.
         # For each detection, the description has the format: [image_id, label, conf, x_min, y_min, x_max, y_max]
         # An every detection is a vector [imageId, classId, conf, x, y, X, Y]
         for detection in out.reshape(-1, 7):
-            image_id = detection[0]
-            label = detection[1]
+            # image_id = detection[0]
+            # label = detection[1]
             conf = detection[2]
 
-            if conf < 0.99:
+            if conf < 0.95:
                 continue
 
-            print('conf: ', conf, ' image_id: ', image_id, ' label: ', label)
+            listDetections.append(detection)
+
+        print("listDetections: ", len(listDetections))
+
+        for detection in listDetections:
+            # print('conf: ', conf, ' image_id: ', image_id, ' label: ', label)
             xmin = int(detection[3] * frame.shape[1])
             ymin = int(detection[4] * frame.shape[0])
             xmax = int(detection[5] * frame.shape[1])
             ymax = int(detection[6] * frame.shape[0])
 
-            coord = xmin, ymin, xmax, ymax
-            calculate_centr(coord)
-            print('coord: ', coord)
+            coord = xmin, xmax, ymin, ymax
+            center = calculate_centr_cut(coord)
+            # print('coord: ', coord)
+            print('calculate_centr: ', center)
 
-
-
+            if len(array_center_car) == 0:
+                array_center_car = np.append(array_center_car, center)
+                print("array_center_car 1: ", array_center_car[0])
+            # elif center[0] == array_center_car[0] || center[1] == array_center_car[1]:
+            #      print("entrou 2")
+            #      array_center_car = np.append(array_center_car, center)
 
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), vColor, rectThinkness)
 
             stop = timeit.default_timer()
             time_cascade = round((stop - start) * 1000, 1)
-            print('Time:', time_cascade, 'ms')
+            # print('Time:', time_cascade, 'ms')
             ############## Time ##############
-            # print('#######################\n')
-            putText(frame, str(label), xmin, ymin, color_font)
-            putTextPrecision(frame, conf, xmax, ymax, (ymax-ymin), color_red)
+            print('#######################\n')
+            # putText(frame, str(label), xmin, ymin, color_font)
+            # Print do centro
+            center = int(center[0]), int(center[1])
+            frame = cv2.circle(frame, center, radius, color_red, thickness)
+            # putTextPrecision(frame, conf, xmax, ymax, (ymax-ymin), color_red)
 
+        print("array_center_car: ", array_center_car)
         # -- Print img gray
-        cv2.imshow('cur', cuttImg)
-        showImg = imutils.resize(frame, height=800)
-        cv2.imshow("showImg", showImg)
+        if cuttImg is not None:
+            cv2.imshow('cur', cuttImg)
+            showImg = imutils.resize(frame, height=800)
+            cv2.imshow("showImg", showImg)
 
 
         if cv2.waitKey(16) & 0xFF == ord('q'):
